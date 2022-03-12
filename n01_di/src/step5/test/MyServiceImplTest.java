3e9a22f33dec1b19ce17aa91a6f5070725bce2ad
chunkind <chunkind@naver.com>
2022-03-12 19:08:07 +0900

    정리

--- n01_di/src/step5/test/MyServiceImplTest.java
+++ n01_di/src/step5/test/MyServiceImplTest.java
@@ -18,32 +18,32 @@ import step5.MyServiceImpl;
  * 2. Resource를 인자값으로 BeanFactory를 생성
  * 3. 빈을 하나 얻어옴
  * 4. 빈의 비지니스 로직을 호출함
- *     Hello MyServiceImpl!을 호출함 
- *     
- *     BeanFactory의 자식 컨테이너인 ApplicationContext :
- *     pre Loading
- *     빈 설정문서를 읽어 들임과 동시에 빈을 생성한다 
- *     즉 생성하고나서 요펑이 들어올 때까지 빈을 보관하는 역할까지 한다는 뜻
- *     서블릿에서 Ready On 과정의 기능이 먼저 작동되기 위해서 
- *     load-startup 태그를 기술했던 것과 거의 동일한 원리이다. 
- *     
- *     두 컨테이너의 공통점은 빈을 싱글톤으로 만들고 관리한다는 점.
+ *  Hello MyServiceImpl!을 호출함 
+ *
+ *  BeanFactory의 자식 컨테이너인 ApplicationContext :
+ *  pre Loading
+ *  빈 설정문서를 읽어 들임과 동시에 빈을 생성한다 
+ *  즉 생성하고나서 요펑이 들어올 때까지 빈을 보관하는 역할까지 한다는 뜻
+ *  서블릿에서 Ready On 과정의 기능이 먼저 작동되기 위해서 
+ *  load-startup 태그를 기술했던 것과 거의 동일한 원리이다. 
+ *
+ *  두 컨테이너의 공통점은 빈을 싱글톤으로 만들고 관리한다는 점.
  */
 public class MyServiceImplTest {
 	public static void main(String[] args) {
-		//pre Loading이 되도록 로직을 다시 작성 
+		//1.pre Loading
 		System.out.println("1.공장을 생성합니다...");
 		ApplicationContext factory = new FileSystemXmlApplicationContext("src/step5/msg.xml");
-		
+
 		System.out.println("2.getBean() call...");
-		MyService service1 =factory.getBean("msg2", MyService.class);
+		MyService service1 =factory.getBean("msg", MyService.class);
 		MyService service2 =factory.getBean("msg2", MyService.class);
 		System.out.println(service1==service2);//true 
-		
+
 		System.out.println("3.Biz Logic call...");
 		System.out.println(service1.printMsg());
-		
-		//2.
+
+		//2.Lazy Loading
 		Resource r = new FileSystemResource("src/step4/msg.xml");
 		BeanFactory factory2 = new XmlBeanFactory(r);
 		
