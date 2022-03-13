66f6751dd5a9c532b5880c97f832ed033362224c
chunkind <chunkind@naver.com>
2022-03-13 18:42:52 +0900

    정리

--- n16_spring_core/src/spring/service/dice/test/DiceTestApp01.java
+++ n16_spring_core/src/spring/service/dice/test/DiceTestApp01.java
@@ -7,14 +7,12 @@ import spring.service.dice.play.Player01;
  * ㅇ DiceA / Player01 Test 하는 Application 
  */
 public class DiceTestApp01 {
-	///Main Method
+	//Main Method
 	public static void main(String[] args){
-		
 		Player01 player01 = new Player01();
 		player01.playDice(3);
 		System.out.println("=======================");
 		System.out.println("선택된 주사위 수의 통합은 : "+player01.getTotalValue());
 		System.out.println("=======================");
-		
 	}
 }
